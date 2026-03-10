657a2ddbd669ae0c146d932478b2373c489076b7
Jacmanzano <carlos.manzano.regalado@gmail.com>
2026-03-10 18:01:17 +0100

    Se cambia la clase Gestion, se añade tema y se crea panel de botones

--- App/GYMSTATS/src/Main/Gestion.java
+++ App/GYMSTATS/src/Main/Gestion.java
@@ -7,30 +7,33 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import com.formdev.flatlaf.*;
+import java.awt.GridLayout;
 
 public class Gestion extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField dni;
-	private JTextField nom;
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Gestion frame = new Gestion();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		try {
+			 FlatDarkLaf.setup();
+				Gestion frame = new Gestion();
+				frame.setVisible(true);
+		} catch(Exception pantalla) {
+			JOptionPane.showMessageDialog(null, "No se ha podido cargar, vuelve a intentarlo.");
+		}
 	}
 
 	public Gestion() {
+		setTitle("Menú Principal");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -38,33 +41,44 @@ public class Gestion extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		//LABEL TITULO
 		JLabel Titulo = new JLabel("GYMSTATS");
 		Titulo.setFont(new Font("Segoe UI", Font.BOLD, 18));
-		Titulo.setBounds(10, 11, 114, 22);
+		Titulo.setBounds(52, 10, 114, 22);
 		contentPane.add(Titulo);
 		
-		JLabel txt_altaUser = new JLabel("Alta Cliente:");
-		txt_altaUser.setFont(new Font("Tahoma", Font.BOLD, 11));
-		txt_altaUser.setBounds(10, 44, 104, 14);
-		contentPane.add(txt_altaUser);
+		//PANEL DE BOTONES
+		JPanel panelBotones = new JPanel();
+		panelBotones.setBounds(30, 43, 143, 175);
+		contentPane.add(panelBotones);
+		panelBotones.setLayout(new GridLayout(5, 1, 0, 0));
 		
-		JLabel txt_dni = new JLabel("DNI:");
-		txt_dni.setBounds(10, 66, 29, 14);
-		contentPane.add(txt_dni);
+		//BOTON CREAR EMPLEADO
+		JButton btnCrearEmpleado = new JButton("Nuevo Empleado");
+		btnCrearEmpleado.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		panelBotones.add(btnCrearEmpleado);
 		
-		dni = new JTextField();
-		dni.setBounds(39, 63, 86, 20);
-		contentPane.add(dni);
-		dni.setColumns(10);
+		//BOTON MOSTRAR MEMBRESIAS
+		JButton btnMostrarMembresias = new JButton("Mostrar Membresias");
+		panelBotones.add(btnMostrarMembresias);
+		btnMostrarMembresias.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		
-		JLabel txt_nom = new JLabel("Nombre:");
-		txt_nom.setBounds(10, 94, 48, 14);
-		contentPane.add(txt_nom);
-		
-		nom = new JTextField();
-		nom.setColumns(10);
-		nom.setBounds(68, 91, 86, 20);
-		contentPane.add(nom);
+		//BOTON CREAR CLIENTE
+		JButton btnCrearCliente = new JButton("Nuevo Cliente");
+		panelBotones.add(btnCrearCliente);
+		btnCrearCliente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 
 	}
 }
