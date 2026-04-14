4f851645c7628d575c5bc22535d3224d901da0aa
Jacmanzano <carlos.manzano.regalado@gmail.com>
2026-04-14 17:58:28 +0200

    Añadido titulo a la ventana principal, titulo, colores y panel

--- App/GYMSTATS/src/Gestion.java
+++ App/GYMSTATS/src/Gestion.java
@@ -7,62 +7,42 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
+import java.awt.Color;
 
 public class Gestion extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField dni;
-	private JTextField nom;
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Gestion frame = new Gestion();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		Gestion frame = new Gestion();
+		frame.setVisible(true);
 	}
 
 	public Gestion() {
+		setTitle("GymStats");
+		setBackground(Color.BLACK);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 494, 335);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.BLACK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel Titulo = new JLabel("GYMSTATS");
-		Titulo.setFont(new Font("Segoe UI", Font.BOLD, 18));
-		Titulo.setBounds(10, 11, 114, 22);
-		contentPane.add(Titulo);
+		//PANELES
+		JPanel pnl_Opciones = new JPanel();
+		pnl_Opciones.setBackground(Color.GRAY);
+		pnl_Opciones.setBorder(new EmptyBorder(0, 0, 0, 0));
+		pnl_Opciones.setBounds(10, 112, 460, 176);
+		contentPane.add(pnl_Opciones);
 		
-		JLabel txt_altaUser = new JLabel("Alta Cliente:");
-		txt_altaUser.setFont(new Font("Tahoma", Font.BOLD, 11));
-		txt_altaUser.setBounds(10, 44, 104, 14);
-		contentPane.add(txt_altaUser);
+		//LABELS
+		JLabel lblTitulo = new JLabel("GymStats");
+		lblTitulo.setFont(new Font("Segoe UI", Font.ITALIC, 28));
+		lblTitulo.setBackground(Color.WHITE);
+		lblTitulo.setBounds(10, 10, 238, 49);
+		contentPane.add(lblTitulo);
 		
-		JLabel txt_dni = new JLabel("DNI:");
-		txt_dni.setBounds(10, 66, 29, 14);
-		contentPane.add(txt_dni);
-		
-		dni = new JTextField();
-		dni.setBounds(39, 63, 86, 20);
-		contentPane.add(dni);
-		dni.setColumns(10);
-		
-		JLabel txt_nom = new JLabel("Nombre:");
-		txt_nom.setBounds(10, 94, 48, 14);
-		contentPane.add(txt_nom);
-		
-		nom = new JTextField();
-		nom.setColumns(10);
-		nom.setBounds(68, 91, 86, 20);
-		contentPane.add(nom);
-
 	}
 }
